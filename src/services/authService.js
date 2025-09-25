1d4b65f7fcde4c00717d8a49bb17b505a2fc83e1
Ronit Kumar Sahu <145933979+ronit0000@users.noreply.github.com>
2025-09-25 16:10:14 +0530

    Fix API_URL to use full DNS with http:// prefix
    Updated API_URL to include http:// prefix for proper requests.

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
-const API_URL = "ec2-54-236-7-30.compute-1.amazonaws.com:8081/auth";
+// Use full public DNS with http:// prefix
+const API_URL = "http://ec2-54-236-7-30.compute-1.amazonaws.com:8081/auth";
 
 export const login = async (username, password) => {
   const response = await axios.post(`${API_URL}/login`, { username, password });
+  // Store token safely
   localStorage.setItem("token", response.data);
   return response.data;
 };
